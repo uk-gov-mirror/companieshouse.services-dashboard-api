e51bf10b35fcec3d23a91de1ae720ac01547fa38
AMoretonCH <amoreton@companieshouse.gov.uk>
2026-07-24 10:13:14 +0100

    Coverage

--- src/test/java/uk/gov/companieshouse/servicesdashboardapi/model/dao/MongoGitInfoTest.java
+++ src/test/java/uk/gov/companieshouse/servicesdashboardapi/model/dao/MongoGitInfoTest.java
@@ -1,7 +1,9 @@
 package uk.gov.companieshouse.servicesdashboardapi.model.dao;
-
+import java.util.Date;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,15 +11,23 @@ class MongoGitInfoTest {
     @Test
     void shouldCreateMongoGitInfoWithValues() {
         MongoGitInfo mongoGitInfo = new MongoGitInfo();
+        MongoGitReleaseInfo releaseInfo = new MongoGitReleaseInfo();
+        releaseInfo.setVersion("1.0.0");
+        
+        List<MongoGitReleaseInfo> releases = List.of(releaseInfo);
+
         mongoGitInfo.setRepo("repo");
         mongoGitInfo.setLang("lang");
         mongoGitInfo.setOwner("owner");
         mongoGitInfo.setServiceArea("serviceArea");
+        mongoGitInfo.setReleases(releases);
 
         assertNotNull(mongoGitInfo);
         assertEquals("repo", mongoGitInfo.getRepo());
         assertEquals("lang", mongoGitInfo.getLang());
         assertEquals("owner", mongoGitInfo.getOwner());
         assertEquals("serviceArea", mongoGitInfo.getServiceArea());
+        assertEquals(releases, mongoGitInfo.getReleases());
+        assertEquals("{r:repo, o:owner, sa:serviceArea, [l:[{v:1.0.0,d:null}]]}", mongoGitInfo.toString());
     }
 }
